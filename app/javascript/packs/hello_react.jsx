5757a9f5a7cabd19ee789426517d65d00d503d7f
Philippe Despreaux <despreaux.philippe@wanadoo.fr>
2018-05-01 20:30:10 +0200

    tried a controlled component

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -83,4 +83,37 @@ class GeoLocs extends React.Component {
   }
 }
 
-ReactDOM.render(<GeoLocs />, document.getElementById('root'));
+// ReactDOM.render(<GeoLocs />, document.getElementById('root'));
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<NameForm />, document.getElementById('root'));
